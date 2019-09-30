d5377d3fe847672de5b360e9b8b6026372bee345
Mathew Gitata <gitataIT@gmail.com>
2019-09-30 15:02:50 +0300

    Add routing to enable user navigate from landing page to user profile search page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
+import { UserProfileComponent } from './component/user-profile/user-profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'search-page', component: UserProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

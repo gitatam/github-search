df1d848196d68a73209025eb0e8a24c471f93191
Mathew Gitata <gitataIT@gmail.com>
2019-09-30 14:40:31 +0300

    Add a landing page to route to the search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './component/user-profile/user-profile.component';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
